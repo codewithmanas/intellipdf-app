6e5ae5944cb6965d7c23e50fcabbde7c4b608667
Manas Ranjan Adha <manas865873@gmail.com>
2025-03-15 11:54:38 +0530

    implement pdf view

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //    turbo: {
+  //       resolveAlias: {
+  //        canvas: './empty-module.ts',
+  //       },
+  //     },
+  //  },
+  // swcMinify: false,
   images: {
     remotePatterns: [
       {
