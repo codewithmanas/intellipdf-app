4d9ec084bedef88e3a4631f617e3f90254688851
CodeWithManas <manas865873@gmail.com>
2026-03-25 11:06:06 +0530

    add experimental turbo for canvas

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // reactStrictMode: false,
   /* config options here */
-  // experimental: {
-  //    turbo: {
-  //       resolveAlias: {
-  //        canvas: './empty-module.ts',
-  //       },
-  //     },
-  //  },
+  experimental: {
+     turbo: {
+        resolveAlias: {
+         canvas: './empty-module.ts',
+        },
+      },
+   },
   // swcMinify: false,
   images: {
     remotePatterns: [
