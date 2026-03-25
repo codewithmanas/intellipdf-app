279e889c8b14100700cd07afd3f0c6b5843671e5
CodeWithManas <manas865873@gmail.com>
2026-03-25 12:41:43 +0530

    convert serviceKey json to .env

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -3,12 +3,26 @@ import { App, cert, getApp, getApps, initializeApp, ServiceAccount } from "fireb
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-import serviceAccountKey from "@/serviceAccountKey.json";
+const serviceAccount = {
+  type: process.env.FIREBASE_SERVICE_KEY_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+}
+
+
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert(serviceAccountKey as ServiceAccount),
+    credential: cert(serviceAccount as ServiceAccount),
   });
 } else {
   app = getApp();
